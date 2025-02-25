d1115758ff59b711ab6e2968340fb94cd5542b2e
Aniket Patra <142705945+Aniketpatra007@users.noreply.github.com>
2025-02-25 18:11:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello");      //we are using nodejs, not browser
+//code readibility should be high
+console.log(3 + 3);
+console.log("aniket");
+
+let name = "Aniket";
+let age = 20;
+let isLoggedIn = false;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);               //object
+console.log(typeof undefined);          //undefined
