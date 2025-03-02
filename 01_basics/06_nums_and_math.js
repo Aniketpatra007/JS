bcfe3bd1cf16b6b1c9dc8c8957e251fd908be0c8
Aniket Patra <142705945+Aniketpatra007@users.noreply.github.com>
2025-03-02 20:18:20 +0000

    numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,15 @@
+const score = 100;
+console.log(score);
+
+const balance = new Number(100)
+console.log(typeof balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.89906;
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
